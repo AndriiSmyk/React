243c6c08426b229c45560b35939e26ada2082ac4
zav0dila <andriysmyk2003@gmail.com>
2023-02-25 18:37:30 +0200

    lesson 1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import Header from "./components/Header/Header";
+import ProductList from "./components/ProductList/ProductList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [products, setProducts] = useState(
+        [
+            {id: 1, title: "Product 1", price: 899},
+            {id: 2, title: "Product 2", price: 982},
+            {id: 3, title: "Product 3", price: 322},
+            {id: 4, title: "Product 4", price: 763},
+            {id: 5, title: "Product 5", price: 389},
+        ]
+    )
+
+    const deleteProduct = (productId) =>{
+        const newProducts = products.filter(product => product.id!==productId);
+        setProducts(newProducts);
+    }
+
+    return (
+        <div>
+            <Header/>
+            <ProductList products={products} deleteProduct={deleteProduct}/>
+        </div>
+    );
 }
 
 export default App;
