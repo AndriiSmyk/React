5e8262f1e9895ab28089419ac973c21e2748a65f
zav0dila <andriysmyk2003@gmail.com>
2023-03-05 17:04:03 +0200

    lesson 3 done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
-import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Header from "./components/Header/Header";
 import ProductList from "./components/ProductList/ProductList";
 import About from "./components/About/About";
@@ -10,31 +8,18 @@ import AddProduct from "./components/AddProduct/AddProduct";
 import EditProduct from "./components/EditProduct/EditProduct";
 
 function App() {
-    const [products, setProducts] = useState(
-        [
-            {id: 1, title: "Product 1", price: 899},
-            {id: 2, title: "Product 2", price: 982},
-            {id: 3, title: "Product 3", price: 322},
-            {id: 4, title: "Product 4", price: 763},
-            {id: 5, title: "Product 5", price: 389},
-        ]
-    )
 
-    const deleteProduct = (productId) => {
-        const newProducts = products.filter(product => product.id !== productId);
-        setProducts(newProducts);
-    }
 
     return (
         <div>
             <Header/>
             <BrowserRouter>
                 <Routes>
-                    <Route path='/' element={<ProductList products={products} deleteProduct={deleteProduct}/>}/>
+                    <Route path='/' element={<ProductList/>}/>
                     <Route path='/about' element={<About/>}/>
                     <Route path='/contact' element={<Contact/>}/>
                     <Route path='/add' element={<AddProduct/>}/>
-                    <Route path='/edit' element={<EditProduct/>}/>
+                    <Route path='/edit/:id' element={<EditProduct/>}/>
 
                 </Routes>
             </BrowserRouter>
